89b0ba3732643098f18743a40be9e8d110c8375f
apurvarai <86016272+apurvarai@users.noreply.github.com>
2026-07-05 22:38:13 +0530

    Add: plus-one - submission-1

---
+++ Data Structures & Algorithms/plus-one/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n=len(digits)
+        res=[]
+        if all(x==9 for x in digits):
+            res.append(1)
+            for j in range(n):
+                res.append(0)
+            return res
+
+        
+        if digits[n-1]==9:
+            last=n-1
+            for j in range(n-2,0,-1):
+                if digits[j]==9:
+                    last=j
+                else:
+                    break
+            cnt=0
+            print(last)
+            while cnt<last and last-cnt>1:
+                res.append(digits[cnt])
+                cnt+=1
+            res.append(digits[cnt]+1)
+            print(res)
+            while cnt<len(digits)-1:
+                res.append(0)
+                cnt+=1
+
+        else:
+            i=0
+            while i<n-1:
+                res.append(digits[i])
+                i+=1
+            res.append(digits[n-1]+1)
+        return res
