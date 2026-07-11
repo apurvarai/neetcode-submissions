158282d2f5572a8ff888467e7057683212257bfa
apurvarai <86016272+apurvarai@users.noreply.github.com>
2026-07-12 03:07:19 +0530

    Add: lemonade-change - submission-0

---
+++ Data Structures & Algorithms/lemonade-change/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        ##we keep 3 variables to store current count of 5,10,20 notes
+        # for 10, cnt2>0 : for 20, cnt2>0 and cnt3>0
+        n=len(bills)
+        cnt1,cnt2,cnt3=0,0,0
+        for i in bills:
+            if i==5:
+                cnt1+=1
+            elif i==10:
+                cnt2+=1
+                if cnt2<=0:
+                    return False
+            else:
+                cnt3+=1
+                if cnt2<=0 or cnt3<=0:
+                    return False
+        return True
+        
