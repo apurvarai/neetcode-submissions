54face1d8b0c44334ec60c7479fcfa5b7ee6cb09
apurvarai <86016272+apurvarai@users.noreply.github.com>
2026-07-16 21:07:23 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        #use kahn's algo and just append the node dequed in the res
+        n=numCourses
+        edges=prerequisites
+        res=[]
+        indeg=[0]*n
+        adj=[[] for _ in range(n)]
+        for u,v in edges: #v->u
+            indeg[u]+=1
+            adj[v].append(u)
+        q=deque()
+        for i in range(n):
+            if indeg[i]==0:
+                q.append(i)
+        while q:
+            node=q.popleft()
+            res.append(node)
+            for i in adj[node]:
+                indeg[i]-=1
+                if indeg[i]==0:
+                    q.append(i)
+        return res
